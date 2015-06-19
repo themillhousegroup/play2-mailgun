e304902c334f57fbf080afb40ec07907993a43f5
John Marshall <john@themillhousegroup.com>
2015-06-19 13:08:25 +1000

    Start to wire in Play's WS

--- src/main/scala/com/themillhousegroup/play2/mailgun/EmailService.scala
+++ src/main/scala/com/themillhousegroup/play2/mailgun/EmailService.scala
@@ -4,8 +4,12 @@ import play.api.libs.concurrent.Execution.Implicits._
 import play.api.{ Logger, Play }
 import scala.concurrent.Future
 import org.apache.commons.lang3.StringUtils
+import play.api.http._
 import play.api.libs.ws._
 import play.api.Play.current
+import com.ning.http.client.FluentCaseInsensitiveStringsMap
+import com.ning.http.multipart._
+import java.io.ByteArrayOutputStream
 
 import com.ning.http.multipart.{ FilePart, MultipartRequestEntity, Part }
 
@@ -19,27 +23,36 @@ class EmailService(val mailgunApiKey: String, val defaultSender: Option[String])
   //client.addFilter(new HTTPBasicAuthFilter("api", mailgunApiKey))
   //val webResource = client.resource(mailgunApiUrl)
 
-  def send(message: EssentialEmailMessage): Future[Option[String]] = Future {
+  def send(message: EssentialEmailMessage): Future[Option[String]] = {
 
     if (defaultSender.isEmpty && message.from.isEmpty) {
-      Future.failed(new IllegalStateException("From: field is None and no default sender configured"))
+      Future.failed[Option[String]](new IllegalStateException("From: field is None and no default sender configured"))
+    } else {
+      val sender = message.from.getOrElse(defaultSender.get)
+
+      //    val logo = Play.getExistingFile("/public/images/logo.png").get
+      //    form.bodyPart(new FileDataBodyPart("inline", logo, MediaType.APPLICATION_OCTET_STREAM_TYPE))
+
+      // Use the Ning AsyncHttpClient multipart class to get the bytes
+      val parts = Array[Part](
+        new StringPart("from", sender),
+        new StringPart("to", message.to),
+        new StringPart("subject", message.subject),
+        new StringPart("text", message.text),
+        new StringPart("html", message.html.toString())
+      )
+      //      new FilePart("attachment", file)
+
+      val mpre = new MultipartRequestEntity(parts, new FluentCaseInsensitiveStringsMap)
+      val baos = new ByteArrayOutputStream
+      mpre.writeRequest(baos)
+      val bytes = baos.toByteArray
+      val contentType = mpre.getContentType
+
+      ws.post(bytes)(Writeable.wBytes, ContentTypeOf(Some(contentType))).map { response =>
+        Option(StringUtils.trimToNull(message.to))
+
+      }
     }
-
-    val sender = message.from.getOrElse(defaultSender)
-
-    /*
-    val form = new FormDataMultiPart
-    form.field("from", message.from)
-    form.field("to", message.to)
-    form.field("subject", message.subject)
-    form.field("text", message.text)
-    form.field("html", message.html.toString())
-    //    val logo = Play.getExistingFile("/public/images/logo.png").get
-    //    form.bodyPart(new FileDataBodyPart("inline", logo, MediaType.APPLICATION_OCTET_STREAM_TYPE))
-
-   // webResource.`type`(MediaType.MULTIPART_FORM_DATA_TYPE).post(form)
-	*/
-
-    Option(StringUtils.trimToNull(message.to))
   }
 }
