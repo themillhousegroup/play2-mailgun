f7bdf3c4d8d8833f78362c71c1e8feccaab3053c
John Marshall <john@themillhousegroup.com>
2015-06-24 15:08:18 +1000

    Building reusable elements for sending tests

--- src/test/scala/com/themillhousegroup/play2-mailgun/EmailServiceSpec.scala
+++ src/test/scala/com/themillhousegroup/play2-mailgun/EmailServiceSpec.scala
@@ -12,18 +12,50 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import org.apache.commons.fileupload._
 import com.ning.http.multipart._
+import org.jboss.netty.handler.codec.http.multipart.FileUpload
+import scala.Predef._
+import scala.Some
 
 class EmailServiceSpec extends Specification with Mockito {
   val timeout = Duration(10, "seconds")
   val mockWS = mock[WSRequestHolder]
   val mockResponse = mock[WSResponse]
   mockResponse.json returns Json.obj("message" -> "OK", "id" -> "abc123")
+  mockResponse.status returns 200
   mockWS.withAuth(any[String], any[String], any[WSAuthScheme]) returns mockWS
   mockWS.post[Array[Byte]](any[Array[Byte]])(any[Writeable[Array[Byte]]], any[ContentTypeOf[Array[Byte]]]) returns Future.successful(mockResponse)
 
   val noSenderEmailMessage = EmailMessage(None, "to@to.com", "subject", "text", Html("<em>text</em>"))
   val senderEmailMessage = EmailMessage(Some("from@from.com"), "to@to.com", "subject", "text", Html("<em>text</em>"))
   val emailService = new MailgunEmailService("apiKey", None)(mockWS)
+
+  def parsedMailgunRequest(ws: WSRequestHolder) = {
+    import scala.collection.JavaConverters._
+
+    val byteCaptor = ArgumentCaptor.forClass(classOf[Array[Byte]])
+    val contentTypeCaptor = ArgumentCaptor.forClass(classOf[ContentTypeOf[Array[Byte]]])
+
+    there was one(mockWS).post(byteCaptor.capture())(any[Writeable[Array[Byte]]], contentTypeCaptor.capture())
+    val theBytes = byteCaptor.getValue
+    theBytes must not beNull
+
+    println(new String(theBytes))
+
+    val fu = new org.apache.commons.fileupload.FileUpload(new org.apache.commons.fileupload.disk.DiskFileItemFactory())
+    val ctx = new UploadContext {
+      def getCharacterEncoding = "UTF-8"
+
+      def getContentType = contentTypeCaptor.getValue.mimeType.get
+
+      def getContentLength = contentLength.toInt
+      def contentLength = theBytes.length
+
+      def getInputStream: java.io.InputStream = {
+        new java.io.ByteArrayInputStream(theBytes)
+      }
+    }
+    fu.parseRequest(ctx).asScala
+  }
 
   "EmailService" should {
     "Bomb out if no default from address and none in supplied message" in {
@@ -34,31 +66,7 @@ class EmailServiceSpec extends Specification with Mockito {
       val response = Await.result(emailService.send(senderEmailMessage), timeout)
       response must beEqualTo(MailgunResponse("OK", "abc123"))
 
-      val byteCaptor = ArgumentCaptor.forClass(classOf[Array[Byte]])
-      val contentTypeCaptor = ArgumentCaptor.forClass(classOf[ContentTypeOf[Array[Byte]]])
-
-      val expected = List[Byte](77, 77, 88, 88, 99).toArray
-      there was one(mockWS).post(byteCaptor.capture())(any[Writeable[Array[Byte]]], contentTypeCaptor.capture())
-
-      val theBytes = byteCaptor.getValue
-      println(new String(theBytes))
-
-      theBytes must not beNull
-
-      val fu = new FileUpload(new org.apache.commons.fileupload.disk.DiskFileItemFactory())
-      val ctx = new UploadContext {
-        def getCharacterEncoding = "UTF-8"
-
-        def getContentType = contentTypeCaptor.getValue.mimeType.get
-
-        def getContentLength = contentLength.toInt
-        def contentLength = theBytes.length
-
-        def getInputStream: java.io.InputStream = {
-          new java.io.ByteArrayInputStream(theBytes)
-        }
-      }
-      val multipartItems = fu.parseRequest(ctx)
+      val multipartItems = parsedMailgunRequest(mockWS)
 
       multipartItems.size must beEqualTo(5)
 
