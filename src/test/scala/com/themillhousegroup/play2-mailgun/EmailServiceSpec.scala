c41fa6058a8c2914729f21be89debcc021808828
John Marshall <john@themillhousegroup.com>
2015-06-24 15:22:52 +1000

    Tests for API-triggered Exceptions

--- src/test/scala/com/themillhousegroup/play2-mailgun/EmailServiceSpec.scala
+++ src/test/scala/com/themillhousegroup/play2-mailgun/EmailServiceSpec.scala
@@ -17,25 +17,33 @@ import scala.Predef._
 import scala.Some
 
 class EmailServiceSpec extends Specification with Mockito {
-  val timeout = Duration(10, "seconds")
-  val mockWS = mock[WSRequestHolder]
-  val mockResponse = mock[WSResponse]
-  mockResponse.json returns Json.obj("message" -> "OK", "id" -> "abc123")
-  mockResponse.status returns 200
-  mockWS.withAuth(any[String], any[String], any[WSAuthScheme]) returns mockWS
-  mockWS.post[Array[Byte]](any[Array[Byte]])(any[Writeable[Array[Byte]]], any[ContentTypeOf[Array[Byte]]]) returns Future.successful(mockResponse)
 
   val noSenderEmailMessage = EmailMessage(None, "to@to.com", "subject", "text", Html("<em>text</em>"))
   val senderEmailMessage = EmailMessage(Some("from@from.com"), "to@to.com", "subject", "text", Html("<em>text</em>"))
-  val emailService = new MailgunEmailService("apiKey", None)(mockWS)
 
-  def parsedMailgunRequest(ws: WSRequestHolder) = {
+  def givenAnEmailServiceThatReturns(statusCode: Int, message: String = "OK"): (MailgunEmailService, WSRequestHolder) = {
+    val mockWS = mock[WSRequestHolder]
+    val mockResponse = mock[WSResponse]
+    mockResponse.json returns Json.obj("message" -> message, "id" -> "abc123")
+    mockResponse.status returns statusCode
+    mockWS.withAuth(any[String], any[String], any[WSAuthScheme]) returns mockWS
+    mockWS.post[Array[Byte]](any[Array[Byte]])(any[Writeable[Array[Byte]]], any[ContentTypeOf[Array[Byte]]]) returns Future.successful(mockResponse)
+    (new MailgunEmailService("apiKey", None)(mockWS), mockWS)
+  }
+
+  def whenTheServiceSends(emailService: MailgunEmailService, msg: EssentialEmailMessage) = {
+    val timeout = Duration(10, "seconds")
+
+    Await.result(emailService.send(msg), timeout)
+  }
+
+  def andMailgunShouldReceive(ws: WSRequestHolder): List[org.apache.commons.fileupload.FileItem] = {
     import scala.collection.JavaConverters._
 
     val byteCaptor = ArgumentCaptor.forClass(classOf[Array[Byte]])
     val contentTypeCaptor = ArgumentCaptor.forClass(classOf[ContentTypeOf[Array[Byte]]])
 
-    there was one(mockWS).post(byteCaptor.capture())(any[Writeable[Array[Byte]]], contentTypeCaptor.capture())
+    there was one(ws).post(byteCaptor.capture())(any[Writeable[Array[Byte]]], contentTypeCaptor.capture())
     val theBytes = byteCaptor.getValue
     theBytes must not beNull
 
@@ -54,22 +62,30 @@ class EmailServiceSpec extends Specification with Mockito {
         new java.io.ByteArrayInputStream(theBytes)
       }
     }
-    fu.parseRequest(ctx).asScala
+    fu.parseRequest(ctx).asScala.toList
   }
 
   "EmailService" should {
-    "Bomb out if no default from address and none in supplied message" in {
-      Await.result(emailService.send(noSenderEmailMessage), timeout) must throwAn[IllegalStateException]
+    "Bomb out with IllegalStateException if no default from address and none in supplied message" in {
+      val (emailService, _) = givenAnEmailServiceThatReturns(200)
+      whenTheServiceSends(emailService, noSenderEmailMessage) must throwAn[IllegalStateException]
+    }
+
+    "Bomb out with MailgunSendingException wrapping the message if remote API doesn't return a 200" in {
+      val (emailService, _) = givenAnEmailServiceThatReturns(401, "Unauthorized - No valid API key provided")
+
+      whenTheServiceSends(emailService, senderEmailMessage) must throwAn[MailgunSendingException]("Unauthorized - No valid API key provided")
     }
 
     "Use the sender from the message if supplied" in {
-      val response = Await.result(emailService.send(senderEmailMessage), timeout)
+      val (emailService, mockWS) = givenAnEmailServiceThatReturns(200)
+
+      val response = whenTheServiceSends(emailService, senderEmailMessage)
       response must beEqualTo(MailgunResponse("OK", "abc123"))
 
-      val multipartItems = parsedMailgunRequest(mockWS)
+      val multipartItems = andMailgunShouldReceive(mockWS)
 
       multipartItems.size must beEqualTo(5)
-
     }
   }
 }
