9264667518386dc5dfe267309bb5799a0486b240
John Marshall <john@themillhousegroup.com>
2015-07-01 12:20:08 +1000

    Conform to the Twirl Format interface

--- src/main/scala/com/themillhousegroup/play2/mailgun/templating/DualFormatEmail.scala
+++ src/main/scala/com/themillhousegroup/play2/mailgun/templating/DualFormatEmail.scala
@@ -3,5 +3,14 @@ package com.themillhousegroup.play2.mailgun.templating
 import play.twirl.api._
 
 class DualFormatEmail(buffer: String) extends BufferedContent[DualFormatEmail](scala.collection.immutable.Seq[DualFormatEmail](), buffer) {
-	val contentType = MimeTypes.HTML
+  val contentType = MimeTypes.HTML
+}
+
+object DualFormat extends Format[DualFormatEmail] {
+  def raw(text: String): DualFormatEmail = ??? // new DualFormatEmail(text) // FIXME need to actually do work 
+  def escape(text: String): DualFormatEmail = ??? // new DualFormatEmail(text) // FIXME need to actually do work 
+
+  def empty: com.themillhousegroup.play2.mailgun.templating.DualFormatEmail = ???
+  def fill(elements: scala.collection.immutable.Seq[com.themillhousegroup.play2.mailgun.templating.DualFormatEmail]): com.themillhousegroup.play2.mailgun.templating.DualFormatEmail = ???
+
 }
