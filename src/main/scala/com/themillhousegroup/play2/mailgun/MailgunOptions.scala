8ee2b33f003d868d798c52c63daf360cd6f4ad91
John Marshall <john@themillhousegroup.com>
2015-08-31 11:12:40 +1000

    Supporting Test mode option

--- src/main/scala/com/themillhousegroup/play2/mailgun/MailgunOptions.scala
+++ src/main/scala/com/themillhousegroup/play2/mailgun/MailgunOptions.scala
@@ -1,9 +1,12 @@
 package com.themillhousegroup.play2.mailgun
 
+import com.ning.http.multipart._
+
 sealed trait MailgunOption {
-  def renderAsApiParameter: String
+  def renderAsApiParameter: Part
 }
 
 case object SendInTestMode extends MailgunOption {
-  def renderAsApiParameter: String = "o:testmode=true"
+  def renderAsApiParameter: Part = new StringPart("o:testmode", "true")
 }
+
