cdbaf30dac53c608b58647692417606fa9cbd061
John Marshall <john@themillhousegroup.com>
2015-08-31 11:14:31 +1000

    Support scheduled send option

--- src/main/scala/com/themillhousegroup/play2/mailgun/MailgunOptions.scala
+++ src/main/scala/com/themillhousegroup/play2/mailgun/MailgunOptions.scala
@@ -10,3 +10,6 @@ case object SendInTestMode extends MailgunOption {
   def renderAsApiParameter: Part = new StringPart("o:testmode", "true")
 }
 
+case class ScheduledSendAt(date: java.util.Date) extends MailgunOption {
+  def renderAsApiParameter: Part = new StringPart("o:deliverytime", date.getTime.toString)
+}
