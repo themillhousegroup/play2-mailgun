a2569aa4a7cd095590e07ef3729ca82f6f6c532d
John Marshall <john@themillhousegroup.com>
2015-08-31 10:52:49 +1000

    Start enumerating possible message-sent states

--- src/main/scala/com/themillhousegroup/play2/mailgun/MailgunResponse.scala
+++ src/main/scala/com/themillhousegroup/play2/mailgun/MailgunResponse.scala
@@ -2,7 +2,13 @@ package com.themillhousegroup.play2.mailgun
 
 import play.api.libs.json.Json
 
-case class MailgunResponse(message: String, id: String)
+sealed trait MailgunSentMessageStatus
+
+case object MessageQueued extends MailgunSentMessageStatus
+
+case class MailgunResponse(message: String, id: String) {
+  lazy val status: MailgunSentMessageStatus = MessageQueued
+}
 
 trait MailgunResponseJson {
   implicit val responseReads = Json.reads[MailgunResponse]
