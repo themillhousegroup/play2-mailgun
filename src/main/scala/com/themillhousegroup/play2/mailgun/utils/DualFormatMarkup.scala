2752937646a21217b5823881a8a48603ecabda14
John Marshall <john@themillhousegroup.com>
2015-12-30 20:38:13 +1100

    Relocated the Dual Format stuff as it doesn't require extra deps

--- src/main/scala/com/themillhousegroup/play2/mailgun/utils/DualFormatMarkup.scala
+++ src/main/scala/com/themillhousegroup/play2/mailgun/utils/DualFormatMarkup.scala
@@ -1,31 +1,31 @@
-package com.themillhousegroup.play2.mailgun.templating.utils
+package com.themillhousegroup.play2.mailgun.utils
 
 import scala.xml.Node
 import play.twirl.api.Html
 
 object DualFormatMarkup {
 
-  def h(markup:Node, alternative:String="")(implicit htmlFormat:Boolean):String = {
-    if(htmlFormat) {
+  def h(markup: Node, alternative: String = "")(implicit htmlFormat: Boolean): String = {
+    if (htmlFormat) {
       (markup.toString)
     } else {
       alternative
     }
   }
 
-  def a(target:String, text:String)(implicit htmlFormat:Boolean) = {
-    if(htmlFormat) {
-      <a href={target}>{text}</a>
+  def a(target: String, text: String)(implicit htmlFormat: Boolean) = {
+    if (htmlFormat) {
+      <a href={ target }>{ text }</a>
     } else {
       target
     }
   }
 
-  def em(text:String)(implicit htmlFormat:Boolean) = h(<em>{text}</em>, text)
+  def em(text: String)(implicit htmlFormat: Boolean) = h(<em>{ text }</em>, text)
 
-  def strong(text:String)(implicit htmlFormat:Boolean) = h(<strong>{text}</strong>, text)
+  def strong(text: String)(implicit htmlFormat: Boolean) = h(<strong>{ text }</strong>, text)
 
-  def tt(text:String)(implicit htmlFormat:Boolean) = h(<tt>{text}</tt>, text)
+  def tt(text: String)(implicit htmlFormat: Boolean) = h(<tt>{ text }</tt>, text)
 
-  def br(implicit htmlFormat:Boolean) = h(<br/>)
+  def br(implicit htmlFormat: Boolean) = h(<br/>)
 }
